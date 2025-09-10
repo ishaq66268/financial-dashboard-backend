600acf3118baab47f0532aa4034bc58ba66f3758
ishaq66268 <ishaq66268@gmail.com>
2025-09-10 21:39:12 +0530

    Your commit message describing the changes

--- test_fetch.py
+++
@@ -1,8 +0,0 @@
-from database import entries_collection
-import asyncio
-
-async def fetch_entries():
-    docs = await entries_collection.find({}).to_list(length=100)
-    print("Documents:", docs)
-
-asyncio.run(fetch_entries())
